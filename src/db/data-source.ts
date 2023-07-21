e4ed14fff7c75b3f7ce63c477bab33a414eb385d
oleh19127 <oleh19127@gmail.com>
2023-07-21 15:23:09 +0300

    Update data-source.ts

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -2,10 +2,14 @@ import { DataSource } from "typeorm";
 import { User } from "./entity/User";
 import { Work } from "./entity/Work";
 
+const port = process.env.DATA_BASE_PORT
+  ? parseInt(process.env.DATA_BASE_PORT)
+  : undefined;
+
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DATA_BASE_HOST,
-  port: parseInt(process.env.DATA_BASE_PORT || "5432"),
+  port: port,
   username: process.env.DATA_BASE_USER_NAME,
   password: process.env.DATA_BASE_PASSWORD,
   database: process.env.DATA_BASE_NAME,
